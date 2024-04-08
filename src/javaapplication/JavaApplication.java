796879ba85215d046e351ccead957337aac402a8
djukic03 <djukic.nemanja003@gmail.com>
2024-04-08 23:48:18 +0200

    Dodata linija "Ravna Gora"

--- src/javaapplication/JavaApplication.java
+++ src/javaapplication/JavaApplication.java
@@ -16,6 +16,7 @@ public class JavaApplication {
     public static void main(String[] args) {
         System.out.println("Cica");
         System.out.println("Ravna Gora");
+        
     }
     
 }
